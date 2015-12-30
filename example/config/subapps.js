2b7b21a3e46a336123bb365bf2a557a3252c8e0e
Scott Gress <scottmgress@gmail.com>
2015-12-30 13:35:54 -0600

    Added some comments to subapp config

--- example/config/subapps.js
+++ example/config/subapps.js
@@ -1,11 +1,16 @@
 module.exports.subapps = {
+  // Config for the "blog" subapp, a Sails app living in subapps/blog
   blog: {
     models: {
+      // Map the parent user model to the user model in the blog app
       user: 'user',
+      // Allow the "post" model to be accessible in parent code as `sails.models.post`
       post: {
         expose: true
       },
     },
+    // Map the "main" connection in the blog app (see /example/subapps/blog/config/models.js)
+    // to the localDiskDb connection in the parent app.
     connections: {
       main: 'localDiskDb'
     }
